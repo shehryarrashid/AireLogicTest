e449fbb82c9a36356ddb582ac31064f34f6986a9
Shehryar Rashid <94257069+shehryarrashid@users.noreply.github.com>
2024-02-19 17:33:57 +0000

    IntelliJ Project: Boilerplate

---
+++ AireLogic/src/Main.java
@@ -0,0 +1,8 @@
+// Author:  Shehryar Rashid Mehmood
+// Aire Logic Medical Score Calculation
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AireLogicTest
 Aire Logic's technical test: Medical Store Calculation
+Name: Shehryar Rashid Mehmood
